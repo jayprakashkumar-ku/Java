256af74b3dd8896ac8d37c20858743c7d1aef681
jayprakashkumar-ku <jayprakashkumar31072004@gmail.com>
2025-04-10 18:50:16 +0530

    new change

--- First.java
+++ First.java
@@ -1,1 +1,12 @@
 
+import java.util.Scanner;
+
+class First{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your Name?");
+        String nm=sc.nextLine();
+        System.out.println("welcome " + nm +" to Skill Development Training");
+
+    }
+}
